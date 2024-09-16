72e9758c94941001fb7110d55e6340d6941e78ad
Junior Moreira <jussmor@outlook.es>
2024-09-15 20:58:30 -0500

    feat: fix formatter

--- .formatter.exs
+++ .formatter.exs
@@ -1,12 +1,24 @@
 spark_locals_without_parens = []
 
-
 [
-  import_deps: [:ash_postgres, :ash, :ash_authentication,
-    :ash_authentication_phoenix,:ecto, :ecto_sql, :phoenix, :spark],
+  import_deps: [
+    :ash_postgres,
+    :ash,
+    :ash_authentication,
+    :ash_authentication_phoenix,
+    :ecto,
+    :ecto_sql,
+    :phoenix,
+    :spark
+  ],
   subdirectories: ["priv/*/migrations"],
   plugins: [Spark.Formatter, Phoenix.LiveView.HTMLFormatter],
-  inputs: ["{mix,.formatter}.exs","*.{heex,ex,exs}", "{config,lib,test}/**/*.{heex,ex,exs}", "priv/*/seeds.exs"],
+  inputs: [
+    "{mix,.formatter}.exs",
+    "*.{heex,ex,exs}",
+    "{config,lib,test}/**/*.{heex,ex,exs}",
+    "priv/*/seeds.exs"
+  ],
   locals_without_parens: spark_locals_without_parens,
   export: [
     locals_without_parens: spark_locals_without_parens
